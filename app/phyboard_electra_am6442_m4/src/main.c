50a42851bd0fc033d075e7adeee386d65b41240c
Daniel Schultz <d.schultz@phytec.de>
2024-05-15 14:39:23 +0200

    app: Initial Demo for phyBOARD-Electra M4
    Initial work of a demo firmware for the phyBOARD-Electra M4 board. It
    only includes a GPIO blinky and UART loopback thread.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

---
+++ app/phyboard_electra_am6442_m4/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 PHYTEC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+#include "../../common/heartbeat.h"
+#include "../../common/uart_loopback.h"
+
+K_THREAD_DEFINE(heartbeat_tid, HEARTBEAT_STACKSIZE, heartbeat, NULL, NULL,
+		NULL, HEARTBEAT_PRIORITY, 0, 0);
+
+K_THREAD_DEFINE(uart_loopback_tid, UART_LOOPBACK_STACKSIZE, uart_loopback,
+		NULL, NULL, NULL, UART_LOOPBACK_PRIORITY, 0, 0);
